7ac82c536f12b0097ea82b379301d9a83b473a22
Jacob-213 <jacobms.ti23@utsj.edu.mx>
2024-09-26 18:04:57 -0600

    primer commit

--- middlewares.js
+++ middlewares.js
@@ -1,7 +1,7 @@
 function intMiddleware(req, res, next){
     const now = new Date();
     console.log("Fecha y hora de accesso: ", now);
-    console.log("Has accedido a la ruta", req.path);
+    console.log("Estas en la ruta", req.path);
     next();
 }
 module.exports = intMiddleware;
